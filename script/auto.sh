e94ea81e0d11fe04c38adfbe0ec9891cefa14563
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-11 17:25:37 +0200

    Create auto.sh

---
+++ script/auto.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -v            # print commands before execution, but don't expand env vars in output
+set -o errexit    # always exit on error
+set -o pipefail   # honor exit codes when piping
+set -o nounset    # fail on unset variables
+
+git clone "https://AnimeBack-Bot:$GH_TOKEN@github.com/TaiStudio/animeback-submit" wallpapers
+cd wallpapers
+npm ci
+
+npm start
+
+git config user.email animebot.tai.studio@outlook.fr
+git config user.name AnimeBack-Bot
+git add .
+git commit -am "update wallpapers" --author "AnimeBack-Bot <animebot.tai.studio@outlook.fr>"
+git pull --rebase
+git push origin master
