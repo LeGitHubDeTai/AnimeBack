83355fcc706dc4b86ca6671a11c6f220ab891b89
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-11 17:48:22 +0200

    Update auto.sh

--- script/auto.sh
+++ script/auto.sh
@@ -11,6 +11,12 @@ npm ci
 
 npm start
 
+# bail if nothing changed
+if [ "$(git status --porcelain)" = "" ]; then
+  echo "no new content found; goodbye!"
+  exit
+fi
+
 git config user.email animebot.tai.studio@outlook.fr
 git config user.name AnimeBack-Bot
 git add .
