d693cc1eccb388ec654b2bb33a7d70b1b685acdd
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-05-28 13:13:17 +0200

    add

---
+++ script/getColors.js
@@ -0,0 +1,88 @@
+/*--------------------------------------------------------------------------------------\
+|  _______    _    _____ _             _ _           ________     ___   ___ ___  __     |
+| |__   __|  (_)  / ____| |           | (_)         /  ____  \   |__ \ / _ \__ \/_ |    |
+|    | | __ _ _  | (___ | |_ _   _  __| |_  ___    /  / ___|  \     ) | | | | ) || |    |
+|    | |/ _` | |  \___ \| __| | | |/ _` | |/ _ \  |  | |       |   / /| | | |/ / | |    |
+|    | | (_| | |  ____) | |_| |_| | (_| | | (_) | |  | |___    |  / /_| |_| / /_ | |    |
+|    |_|\__,_|_| |_____/ \__|\__,_|\__,_|_|\___/   \  \____|  /  |____|\___/____||_|    |
+|                                                   \________/                          |
+\--------------------------------------------------------------------------------------*/
+
+var colors = require('colors');
+const fs = require('fs');
+var Jimp = require('jimp');
+const nconf = require('nconf');
+
+var config = `./log/colorsFile.json`;
+nconf.file(config);
+
+const testFolder = './log/preview';
+
+var allBlack = [],
+    allOther = [];
+
+var temp = [];
+var files = getFiles(testFolder);
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        if(dir != `${testFolder}/animals/Categories.json`){
+            if(dir != `${testFolder}/generator`){ //remove
+                if(dir != `${testFolder}/interactive`){
+                    var name = dir + '/' + files[i];
+                    if (fs.statSync(name).isDirectory()){
+                        getFiles(name, files_);
+                        temp = [];
+                    } else {
+                        files_.push(name);
+                        getColor(name);
+                    }
+                }
+            }
+        }
+    }
+    return files_;
+}
+
+function getColor(file){
+    if(file){return;}
+    var black = 0,
+        other = 0;
+    Jimp.read(file, (err, image) => {
+        if (err) throw err;
+
+        try {
+            for(y=0;y<image.getHeight();y++){
+                for(x=0;x<image.getWidth();x++){
+                    var t = image.getPixelColor(x, y);
+        
+                    if(t == 255){
+                        black++;
+                    }
+                    else{
+                        other++;
+                    }
+                    console.log(`Int: ${t}; Black: ${black}; Other: ${other};`);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        finally{
+            if(black > other){
+                console.log('Black Image !'.red);
+                allBlack.push(file.replace('./log', './images'));
+                nconf.set('Black', allBlack);
+                nconf.save();
+            }
+            else{
+                console.log('Best Image !'.green);
+                fs.unlinkSync(file);
+                allOther.push(file.replace('./log', './images'));
+                nconf.set('Other', allOther);
+                nconf.save();
+            }
+        }
+    });
+}
---
+++ script/resizeForTest.js
@@ -0,0 +1,58 @@
+/*--------------------------------------------------------------------------------------\
+|  _______    _    _____ _             _ _           ________     ___   ___ ___  __     |
+| |__   __|  (_)  / ____| |           | (_)         /  ____  \   |__ \ / _ \__ \/_ |    |
+|    | | __ _ _  | (___ | |_ _   _  __| |_  ___    /  / ___|  \     ) | | | | ) || |    |
+|    | |/ _` | |  \___ \| __| | | |/ _` | |/ _ \  |  | |       |   / /| | | |/ / | |    |
+|    | | (_| | |  ____) | |_| |_| | (_| | | (_) | |  | |___    |  / /_| |_| / /_ | |    |
+|    |_|\__,_|_| |_____/ \__|\__,_|\__,_|_|\___/   \  \____|  /  |____|\___/____||_|    |
+|                                                   \________/                          |
+\--------------------------------------------------------------------------------------*/
+
+var colors = require('colors');
+const fs = require('fs');
+const path = require('path');
+const nconf = require('nconf');
+const sharp = require('sharp');
+const imagemin = require('imagemin');
+const testFolder = './images';
+var config = `${testFolder}/categories.json`;
+
+nconf.file(config);
+
+Object.keys(nconf.stores).forEach(function(name){
+    Object.keys(nconf.stores[name].store).forEach(function(test){
+        for(i=0;i<nconf.get(`${test}`).length;i++){
+            var count = nconf.get(`${test}:${i}`).length;
+            if(nconf.get(`${test}:${i}`).slice(count - 3, count) == "mp4"){
+                var fileName = nconf.get(`${test}:${i}`).slice(0, count - 4);
+                if(fs.existsSync(`${testFolder}/preview/${test}/${fileName}.png`)){
+                    resize(`${testFolder}/preview/${test}/${fileName}.png`, 256, 256);
+                }
+            }
+        }
+    })
+});
+
+console.log('Done!'.green);
+
+
+function resize(file, size, size2) {
+    const newFile = file.replace(testFolder, './log');
+  
+    // skip files that are up to date
+    if (
+      fs.existsSync(newFile) &&
+      fs.statSync(newFile).mtime > fs.statSync(file).mtime
+    ) {
+      return Promise.resolve(null)
+    }
+  
+    return (
+      sharp(fs.readFileSync(file))
+        .resize(size, size2, { fit: 'inside' })
+        .toFormat('png')
+        .toBuffer()
+        .then((buf) => imagemin.buffer(buf))
+        .then((buf) => fs.writeFileSync(newFile, buf))
+    )
+}
