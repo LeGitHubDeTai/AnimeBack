617696c11fb8648d2e5d026020e11cfa579740b2
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-06-10 12:29:59 +0200

    🖋 Update

--- script/reset.js
+++ script/reset.js
@@ -12,6 +12,7 @@ var colors = require('colors');
 const rimraf = require('rimraf');
 
 rimraf('./log/colorsFile.json', (err) => console.error('Done !'));
+rimraf('./log/env.json', (err) => console.error('Done !'));
 rimraf('./log/preview/', (err) => console.error('Done !'));
 rimraf('./images/preview/', (err) => console.error('Done !'));
 rimraf('./images/categories.json', (err) => console.error('Done !'));
