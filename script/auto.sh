67d32a0b2e32fd317db6680069ea309ed3c8747e
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2021-06-12 23:21:46 +0200

    🖋 Update

--- script/auto.sh
+++ script/auto.sh
@@ -7,7 +7,7 @@ set -o nounset    # fail on unset variables
 
 git clone "https://AnimeBack-Bot:$GH_TOKEN@github.com/LeGitHubDeTai/AnimeBack/" wallpapers
 cd wallpapers
-npm ci
+npm install
 
 npm start
 
